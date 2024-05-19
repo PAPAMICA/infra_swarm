c77d7419228108b4b070a985469c4c9e50505d5c
Mickael PAPAMICA Asseline <mickael@papamica.com>
2024-05-19 17:18:11 +0200

    fix debian

--- infra/bootstrap/manager.sh
+++ infra/bootstrap/manager.sh
@@ -3,13 +3,3 @@ sudo useradd -m -s /bin/bash ansible
 echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
 mkdir /home/ansible/.ssh
 echo "${var.ansible_publickey}" >> /home/ansible/.ssh/authorized_keys
-# Docker install
-sudo apt update
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker user
